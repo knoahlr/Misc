b7236a3dd9914191213882fab1d0b47dae680ab6
Noah Langat <NoahLangat@cmail.carleton.ca>
2019-08-27 23:13:10 -0400

    Scans Temp during ramp --functional

--- Python/scanTempDuringRamp.py
+++ Python/scanTempDuringRamp.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
 import sys, os
 from enum import Enum
 
@@ -18,18 +20,27 @@ class ChannelSeries:
 		self.ChannelPower = list()
 		self.Temperatures = list()
 		self.Count = 0
+		self.reference = 0
+		self.stdPower = list()
+		self.tempPowerDataframe = None
+
+	def relativePower(self):
+		self.reference = np.mean(self.ChannelPower)
+		self.stdPower = [power - self.reference for power in self.ChannelPower]
+		self.tempPowerDataframe = pd.DataFrame({"Temperature":self.Temperatures, "Relative Power":self.stdPower})
+
+
 
 def isNumber(number):
 	try:
 		float(number)
 		return True
 	except Exception as e:
-		print(e)
 		return False
 
 if __name__ == "__main__":
    
-	filepath = r"C:\Users\lan68400\OneDrive - Lumentum Operations LLC\OCM\OCM ODVT Data\25\1-01551 1234 debug 20190826175205 1-1 ScanDuringRampTest.xlsx" #sys.argv[1]
+	filepath = r"C:\Users\Noah Workstation\OneDrive - Lumentum Operations LLC\OCM\OCM ODVT Data\25\1-01551 1234 debug 20190826175205 1-1 ScanDuringRampTest.xlsx" #sys.argv[1]
 	print(filepath)
 	worksheetDataFrame = pd.read_excel(filepath, sheet_name="Raw Data Sheet")
 	Tables = dict()
@@ -60,21 +71,40 @@ if __name__ == "__main__":
 		for index, row in dataTable.iterrows():
 			
 			rowChannelCenter = row[ColumnNames.Channel_Center.name]
-			if (float(rowChannelCenter)) not in list(ChannelData.keys()):
-				chSeries = ChannelSeries()
-				chSeries.ChannelCenter = rowChannelCenter
-				chSeries.ChannelPower.append(row[ColumnNames.ChannelPower.name])
-				chSeries.Temperatures.append(row[ColumnNames.Temp.name])
-				chSeries.Count += 1
-				ChannelData[rowChannelCenter] = chSeries
-				
-			else:
-				chSeries = ChannelData[rowChannelCenter]
-				chSeries.ChannelPower.append(row[ColumnNames.ChannelPower.name])
-				chSeries.Temperatures.append(row[ColumnNames.Temp.name])
-				chSeries.Count += 1
+			if (float(rowChannelCenter)>186000 and float(rowChannelCenter) <191000):
+				if(float(rowChannelCenter)) not in list(ChannelData.keys()):
+					chSeries = ChannelSeries()
+					chSeries.ChannelCenter = rowChannelCenter
+					chSeries.ChannelPower.append(row[ColumnNames.ChannelPower.name])
+					chSeries.Temperatures.append(row[ColumnNames.Temp.name])
+					chSeries.Count += 1
+					ChannelData[rowChannelCenter] = chSeries
+					
+				else:
+					chSeries = ChannelData[rowChannelCenter]
+					chSeries.ChannelPower.append(row[ColumnNames.ChannelPower.name])
+					chSeries.Temperatures.append(row[ColumnNames.Temp.name])
+					chSeries.Count += 1
 
-	sys.exit()
+	figure = plt.figure()
+	axis = figure.gca()					
+
+	for channel, chanSeries in ChannelData.items():
+		popkeys = []
+		chanSeries.relativePower()
+		if chanSeries.reference > -10 or chanSeries.reference < -25:
+			popkeys.append(channel)
+		else:
+			chanSeries.tempPowerDataframe.sort_values(by=['Temperature'], ascending=True)		
+			axis.plot(chanSeries.tempPowerDataframe["Temperature"], chanSeries.tempPowerDataframe["Relative Power"], ".", label="{0} GHz".format(channel))
+	[ChannelData.pop(keys) for keys in popkeys]
+
+	axis.set_xlabel('Temperature (C)')
+	axis.set_ylabel('Relative Power (dB)')
+	axis.set_title("Continous Scans over Temperature")
+
+	plt.legend(prop={'size': 6})
+	plt.show()
 	print("Done")
 
 
